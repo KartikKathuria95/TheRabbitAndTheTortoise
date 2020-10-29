cc05240ce90098a770862b9039f4ba0dc7906342
Kartik Kathuria (33) <Kartik.kathuria_cs19@gla.ac.in>
2020-10-29 18:27:39 +0530

    Make Tortoise Constructor in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -7,4 +7,8 @@ public class Tortoise implements Runnable {
     private static final int MILESTONES = 5;
 
     private final Thread tortoise;
+
+    public Tortoise() {
+        this.tortoise = new Thread(this, "Tortoise");
+    }
 }
