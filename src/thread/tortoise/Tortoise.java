6b66709a90fa678894048959c4aa9be6444a2cf0
Kartik Kathuria (33) <Kartik.kathuria_cs19@gla.ac.in>
2020-10-29 18:29:19 +0530

    For Loops and Switch Cases in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -15,4 +15,46 @@ public class Tortoise implements Runnable {
     public Thread getTortoise() {
         return tortoise;
     }
+
+    @Override
+    public void run() {
+        for (int index = 0; index < MILESTONES; index++) {
+            switch (index) {
+                case 0:
+                    System.out.println("The Tortoise has started from the start line!");
+                    break;
+                case 1:
+                    System.out.println("The Tortoise has reached the Cool River!");
+                    break;
+                case 2:
+                    System.out.println("The Tortoise has reached the Mountain Hill!");
+                    break;
+                case 3:
+                    System.out.println("The Tortoise has reached the Big Oak Tree!");
+                    System.out.println("The Tortoise is leading and hence is going to sleep!");
+                    try {
+                        Thread.sleep(18000L);
+                    } catch (InterruptedException e) {
+                        System.err.println("The Tortoise's sleep got interrupted!");
+                    }
+                    break;
+                case 4:
+                    System.out.println("The Tortoise has reached the finish line!");
+                    break;
+            }
+            try {
+                Thread.sleep(3000L);
+            } catch (InterruptedException e) {
+                System.err.println("The Tortoise has lost the path!");
+            }
+        }
+        JOptionPane.showConfirmDialog(
+                null,
+                "The Tortoise has completed the race!",
+                "Tortoise",
+                JOptionPane.OK_CANCEL_OPTION
+        );
+
+    }
+}
 }
